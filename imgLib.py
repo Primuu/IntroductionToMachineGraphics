975d27cb4cd167c7db440e3ecc539274ddbc6d7f
Primuu <trentowski.adam@gmail.com>
2022-10-26 16:49:35 +0200

    Lab 2 ended (hsv almost works, hsi not working, hsl almost works)

--- imgLib.py
+++ imgLib.py
@@ -1,13 +1,11 @@
-import math
-
 import matplotlib
 import numpy as np
+
 from matplotlib.image import imread
 from matplotlib.pyplot import imshow
 from matplotlib.image import imsave
 from enum import Enum
-
-from numpy import ndarray
+from typing import Any
 
 
 class ColorModel(Enum):
@@ -22,12 +20,15 @@ class BaseImage:
     data: np.ndarray  # tensor that stores the pixels of image
     color_model: ColorModel  # attribute that stores current color model  of image
 
-    def __init__(self, path: str) -> None:
+    def __init__(self, data: Any, color_model: ColorModel) -> None:
         """
-        initializer that loads an image into the data attribute based on a path
+        initializer that loads an image into the data attribute (based on a path too)
         """
-        self.data = imread(path)
-        self.color_model = ColorModel.rgb
+        if isinstance(data, str):
+            self.data = imread(data)
+        else:
+            self.data = data
+        self.color_model = color_model
 
     def save_img(self, path: str) -> None:
         """
@@ -42,160 +43,163 @@ class BaseImage:
         imshow(self.data)
         matplotlib.pyplot.show()
 
-    #
-    # BaseImage? np.ndarray?
-    #
-    def get_layer(self, layer_id: int) -> 'BaseImage':
+    def get_layer(self, layer_id: int) -> 'np.ndarray':
         """
         method that returns layer with the indicated index
         """
         return self.data[:, :, layer_id]
 
     def to_hsv(self) -> 'BaseImage':
-    # def to_hsv(self) -> ndarray:
         """
-        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsv
-        metoda zwraca nowy obiekt klasy Baseimage zawierajacy obraz w docelowym modelu barw
-
         method that converts image in attribute date to hsv
-        # method returns new object of class BaseImage that stores image in hsv color model
+        method returns new object of class BaseImage that stores image in hsv color model
         """
-        # if !(rgb):
-        #   to_rgb
-        # dtype='uint16'
-        # r = np.array(self.data[:, :, 0])
-        # g = np.array(self.data[:, :, 1])
-        # b = np.array(self.data[:, :, 2])
-        # maximum = np.maximum.reduce([r, g, b], dtype='float')
-        # minimum = np.minimum.reduce([r, g, b], dtype='float')
-        # v = maximum / 255
-        # s = np.where(maximum > 0, 1 - minimum / maximum, 0)
+        if self.color_model != ColorModel.rgb:
+            raise Exception("Color model must be RGB")
+        red_layer = self.get_layer(0) / 255.0
+        green_layer = self.get_layer(1) / 255.0
+        blue_layer = self.get_layer(2) / 255.0
+        M = np.maximum.reduce([red_layer, green_layer, blue_layer])
+        m = np.minimum.reduce([red_layer, green_layer, blue_layer])
+        V = M / 255.0
+        S = np.where(M > 0, 1 - m / M, 0)
+        under_square_expression = np.power(red_layer, 2) + np.power(green_layer, 2) + np.power(blue_layer, 2) - \
+                                  red_layer * green_layer - red_layer * blue_layer - green_layer * blue_layer
+        H = np.where(green_layer >= blue_layer,
+                     np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1),
+                     360 - np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1))
 
-        # h = np.where(g >= b,
-        #              np.cos((r - 0.5 * g - 0.5 * b) / (np.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (
-        #                  -1),
-        #              360 - np.cos(
-        #                  (r - 0.5 * g - 0.5 * b) / (np.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (-1))
-
-        # rf = r.flatten()
-        # gf = g.flatten()
-        # bf = b.flatten()
-        # h = np.empty(0)
-        # for r, g, b in zip(rf, gf, bf):
-        #     if r == 0 & g == 0 & b == 0:
-        #         np.append(h, 0)
-        #     elif g >= b:
-        #         np.append(h, (math.cos(
-        #             (r - 0.5 * g - 0.5 * b) / (math.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (-1)))
-        #     else:
-        #         np.append(h, 360 - math.cos(
-        #             (r - 0.5 * g - 0.5 * b) / (math.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (-1))
-        # h = h.reshape(r.shape)
-
-        # return np.dstack((h, s, v))
-        self.data = self.data.copy()
-        for pixel in self.data:
-            for color in pixel:
-                red, green, blue = color[0], color[1], color[2]
-                M = max(red, green, blue)
-                m = min(red, green, blue)
-                V = M / 255
-                S = 1 - m / M if M > 0 else 0
-                addition = red ** 2 + green ** 2 + blue ** 2
-                subtraction = int(red) * int(green) - int(red) * int(blue) - int(green) * int(blue)
-                additionMinusSubtraction = addition - subtraction
-                if red == 0 & green == 0 & blue == 0:
-                    H = 0
-                elif green >= blue:
-                    H = np.cos((red - 0.5 * green - 0.5 * blue) / np.sqrt(additionMinusSubtraction)) ** (-1)
-                else:
-                    H = 360 - np.cos((red - green / 2 - blue / 2) / np.sqrt(additionMinusSubtraction)) ** (-1)
-                color[0] = H * 100
-                color[1] = S * 100
-                color[2] = V * 100
-
-        self.color_model = ColorModel.hsv
-        return self
-
-
+        return BaseImage(np.dstack((H, S, V)), ColorModel.hsv)
 
     def to_hsi(self) -> 'BaseImage':
         """
-        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsi
-        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        method that converts image in attribute date to hsi
+        method returns new object of class BaseImage that stores image in hsi color model
         """
-        pass
+        if self.color_model != ColorModel.rgb:
+            raise Exception("Color model must be RGB")
+        red_layer = self.get_layer(0) / 255.0
+        green_layer = self.get_layer(1) / 255.0
+        blue_layer = self.get_layer(2) / 255.0
+        M = np.maximum.reduce([red_layer, green_layer, blue_layer])
+        m = np.minimum.reduce([red_layer, green_layer, blue_layer])
+        I = (red_layer + green_layer + blue_layer) / 3.0
+        S = np.where(M > 0, 1 - m / M, 0)
+        under_square_expression = np.power(red_layer, 2) + np.power(green_layer, 2) + np.power(blue_layer, 2) - \
+                                  red_layer * green_layer - red_layer * blue_layer - green_layer * blue_layer
+        H = np.where(green_layer >= blue_layer,
+                     np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1),
+                     360 - np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1))
+
+        return BaseImage(np.dstack((H, S, I)), ColorModel.hsi)
 
     def to_hsl(self) -> 'BaseImage':
         """
-        metoda dokonujaca konwersji obrazu w atrybucie data do modelu hsl
-        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        method that converts image in attribute date to hsl
+        method returns new object of class BaseImage that stores image in hsl color model
         """
-        pass
+        if self.color_model != ColorModel.rgb:
+            raise Exception("Color model must be RGB")
+        red_layer = self.get_layer(0) / 255.0
+        green_layer = self.get_layer(1) / 255.0
+        blue_layer = self.get_layer(2) / 255.0
+        M = np.maximum.reduce([red_layer, green_layer, blue_layer])
+        m = np.minimum.reduce([red_layer, green_layer, blue_layer])
+        d = (M - m) / 255.0
+        L = (M + m) / 510.0
+        S = np.where(L > 0, d / (1 - np.fabs(2 * L - 1)), 0)
+        under_square_expression = np.power(red_layer, 2) + np.power(green_layer, 2) + np.power(blue_layer, 2) - \
+                                  red_layer * green_layer - red_layer * blue_layer - green_layer * blue_layer
+        H = np.where(green_layer >= blue_layer,
+                     np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1),
+                     360 - np.cos((red_layer - 0.5 * green_layer - 0.5 * blue_layer) / np.sqrt(under_square_expression))
+                     ** (-1))
+
+        return BaseImage(np.dstack((H, S, L)), ColorModel.hsl)
+
+    def hsv_to_rgb(self) -> 'BaseImage':
+        """
+        method that converts hsv image in attribute date to rgb
+        method returns new object of class BaseImage that stores image in rgb color model
+        """
+        H = self.get_layer(0)
+        S = self.get_layer(1)
+        V = self.get_layer(2)
+        M = 255 * V
+        m = M * (1 - S)
+        z = (M - m) * (1 - np.fabs(((H / 60) % 2) - 1))
+        R = np.where(H < 60, M, np.where(H < 120, z + m, np.where(H < 240, m, np.where(H < 300, z + m, np.where(
+            H < 360, M, 0)))))
+        G = np.where(H < 60, z + m, np.where(H < 240, M, np.where(H < 360, m, 0)))
+        B = np.where(H < 120, m, np.where(H < 240, z + m, np.where(H < 300, M, np.where(H < 360, z + m, 0))))
+        return BaseImage(np.dstack((R, G, B)), ColorModel.rgb)
+
+    def hsi_to_rgb(self) -> 'BaseImage':
+        """
+        method that converts hsi image in attribute date to rgb
+        method returns new object of class BaseImage that stores image in rgb color model
+        """
+        H = self.get_layer(0)
+        S = self.get_layer(1)
+        I = self.get_layer(2)
+        IS = I * S
+        R = np.where(H == 0, I + 2 * IS, np.where(H < 120, I + IS * np.cos(H) / np.cos(60 - H), np.where(
+            H <= 240, I - IS, np.where(H < 360, I + IS * (1 - np.cos(H - 240) / np.cos(300 - H)), 0))))
+        G = np.where(H == 0, I - IS, np.where(H < 120, I + IS * (1 - np.cos(H) / np.cos(60 - H)), np.where(
+            H == 120, I + 2 * IS, np.where(H < 240, I + IS * np.cos(H - 120) / np.cos(180 - H), np.where(
+                H < 360, I - IS, 0)))))
+        B = np.where(H <= 120, I - IS, np.where(H < 240, I + IS * (1 - np.cos(H - 120) / np.cos(180 - H)), np.where(
+            H == 240, I + 2 * IS, np.where(H < 360, I + IS * np.cos(H - 240) / np.cos(300 - H), 0))))
+
+        return BaseImage(np.dstack((R, G, B)), ColorModel.rgb)
+
+    def hsl_to_rgb(self) -> 'BaseImage':
+        """
+        method that converts hsl image in attribute date to rgb
+        method returns new object of class BaseImage that stores image in rgb color model
+        """
+        H = self.get_layer(0)
+        S = self.get_layer(1)
+        L = self.get_layer(2)
+        d = S * (1 - np.fabs(2 * L - 1))
+        m = 255.0 * (L - 0.5 * d)
+        x = d * (1 - np.fabs((H / 60) % 2) - 1)
+        d255m = d * 255.0 + m
+        x255m = x * 255.0 + m
+        R = np.where(H < 60, d255m, np.where(H < 120, x255m, np.where(H < 240, m, np.where(H < 300, x255m, np.where(
+            H < 360, d255m, 0)))))
+        G = np.where(H < 60, x255m, np.where(H < 120, d255m, np.where(H < 180, d255m, np.where(H < 240, x255m, np.where(
+            H < 360, m, 0)))))
+        B = np.where(H < 120, m, np.where(H < 180, x255m, np.where(H < 240, d255m, np.where(H < 300, d255m, np.where(
+            H < 360, x255m, 0)))))
+        return BaseImage(np.dstack((R, G, B)), ColorModel.rgb)
 
     def to_rgb(self) -> 'BaseImage':
         """
-        metoda dokonujaca konwersji obrazu w atrybucie data do modelu rgb
-        metoda zwraca nowy obiekt klasy image zawierajacy obraz w docelowym modelu barw
+        method that converts image in attribute date to rgb
+        method returns new object of class BaseImage that stores image in rgb color model
         """
-        pass
+        match self.color_model:
+            case ColorModel.hsv:
+                return self.hsv_to_rgb()
+            case ColorModel.hsi:
+                return self.hsi_to_rgb()
+            case ColorModel.hsl:
+                return self.hsl_to_rgb()
+            case _:
+                return self
 
 
-# TESTS
-
-# img_arr = imread('data/lena.jpg')
-
-# imshow(img_arr)
-# matplotlib.pyplot.show()
-
-image_test = BaseImage('data/lena.jpg')
+image_test = BaseImage('data/lena.jpg', ColorModel.rgb)
 image_test.show_img()
-
-image_test.to_hsv()
-image_test.show_img()
-
-
-# testFunkcji = image_test.to_hsv()
-# print(testFunkcji)
-# imshow(testFunkcji)
-# matplotlib.pyplot.show()
-
-# rgb = np.array([[[255, 1, 2], [200, 3, 4]], [[5, 6, 122], [0, 0, 0]]])
-# r = rgb[:, :, 0]
-# g = rgb[:, :, 1]
-# b = rgb[:, :, 2]
-# print(r)
-# print(g)
-# print(b)
-# print()
-
-# r = np.array([[2, 2], [0, 1]], dtype='float')
-# g = np.array([[2, 2], [0, 1]], dtype='float')
-# b = np.array([[1, 3], [4, 0]], dtype='float')
-
-# print(r)
-# print(g)
-# print(b)
-# print()
-
-# rf = r.flatten()
-# gf = g.flatten()
-# bf = b.flatten()
-# h = []
-# for r, g, b in zip(rf, gf, bf):
-#     if r == 0 & g == 0 & b == 0:
-#         h.append(0)
-#     elif g >= b:
-#         h.append(math.cos((r - 0.5 * g - 0.5 * b) / (math.sqrt(r**2 + g**2 + b**2 - r*g - r*b - g*b))) ** (-1))
-#     else:
-#         h.append(360 - math.cos((r - 0.5 * g - 0.5 * b) / (math.sqrt(r**2 + g**2 + b**2 - r*g - r*b - g*b))) ** (-1))
-# print(h)
-#
-# h2 = np.where(g >= b,
-#              np.cos((r - 0.5 * g - 0.5 * b) / (np.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (
-#                  -1),
-#              360 - np.cos(
-#                  (r - 0.5 * g - 0.5 * b) / (np.sqrt(r ** 2 + g ** 2 + b ** 2 - r * g - r * b - g * b))) ** (-1))
-# print(h2)
+x = image_test.to_hsl()
+x.show_img()
+y = x.to_rgb()
+y.show_img()
 
 
